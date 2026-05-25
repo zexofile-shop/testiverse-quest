9889131f8a561aff46261563b86ef32f5c139ff1
zexofile-shop <zexofile@gmail.com>
2026-05-25 14:41:34 +0530

    Added package-lock.json

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -121,3 +121,13 @@ declare module '@tanstack/react-start' {
     config: Awaited<ReturnType<typeof startInstance.getOptions>>
   }
 }
+
+import type { getRouter } from './router.tsx'
+import type { startInstance } from './start.ts'
+declare module '@tanstack/react-start' {
+  interface Register {
+    ssr: true
+    router: Awaited<ReturnType<typeof getRouter>>
+    config: Awaited<ReturnType<typeof startInstance.getOptions>>
+  }
+}
